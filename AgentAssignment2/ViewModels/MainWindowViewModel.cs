f24c253d7222332b4d7bd87cdc6ac2c9b661d0fb
Poul Ejnar Rovsing <per@ece.au.dk>
2022-02-17 11:39:25 +0100

    lab1 Done

---
+++ AgentAssignment2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using AgentAssignment2.Models;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AgentAssignment2.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        ObservableCollection<Agent> agents;
+
+        public MainWindowViewModel()
+        {
+            agents = new ObservableCollection<Agent>();
+            agents.Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
+            agents.Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+            CurrentAgent = agents[0];
+        }
+
+        #region Properties
+
+        Agent? currentAgent = null;
+
+        public Agent CurrentAgent
+        {
+            get { return currentAgent; }
+            set { SetProperty(ref currentAgent, value); }
+        }
+
+        public ObservableCollection<Agent> Agents
+        {
+            get { return agents; }
+            set { SetProperty(ref agents, value); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddNewAgent()
+        {
+            agents.Add(new Agent());
+        }
+
+        #endregion
+    }
+}
--- AgentAssignment2/Views/MainWindow.xaml.cs
+++ AgentAssignment2/Views/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AgentAssignment2.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AgentAssignment2
 {
@@ -23,6 +11,26 @@ namespace AgentAssignment2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxAgents.SelectedIndex > 0)
+                lbxAgents.SelectedIndex = --lbxAgents.SelectedIndex;
+        }
+
+        private void BtnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxAgents.SelectedIndex < lbxAgents.Items.Count - 1)
+                lbxAgents.SelectedIndex = ++lbxAgents.SelectedIndex;
+        }
+
+        private void BtnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as MainWindowViewModel;
+            vm.AddNewAgent();
+            lbxAgents.SelectedIndex = lbxAgents.Items.Count - 1;
+            tbxId.Focus();
         }
     }
 }
